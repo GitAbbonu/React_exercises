6afb8a01c372795e2630bd20ee81ca2cd8af4e04
Cipriano Mereu <ciprianomereu@gmail.com>
2023-02-21 11:23:12 +0100

    fix:exercise 4

---
+++ my-app/src/fileVuoto.js
@@ -0,0 +1,4 @@
+//Consegna:
+// Create a new React application with create-react-app and remove all the code within the src folder.
+
+// Ho lasciato questo file vuoto, perché senza, git non mi caricava la cartella "src" vuota.
