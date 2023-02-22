57d076218225687b496ae8ecfa308bc947efd70f
Cipriano Mereu <ciprianomereu@gmail.com>
2023-02-22 15:36:23 +0100

    add:exercise 5

---
+++ my-app/src/Hello.js
@@ -0,0 +1,11 @@
+// Write a Hello class components that renders the "Hello, World!" message within an h1 tag.
+
+import React from "react";
+
+class Hello extends React.Component {
+  render() {
+    return <h1>"Hello, World!"</h1>;
+  }
+}
+
+export default Hello;
