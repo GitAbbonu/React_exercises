aa38a2d223ceb5289fe5b7ba1dac2062fc3e63cb
Cipriano Mereu <ciprianomereu@gmail.com>
2023-02-22 16:31:53 +0100

    fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import Hello from "./Hello";
 import Welcome from "./Welcome";
 
+// Create a Welcome class component that receives a name prop and renders the Welcome, {name}! message within a p tag. Render this component to you App component, passing it a name prop of your choosing.
+
 function App() {
   return (
     <div>
