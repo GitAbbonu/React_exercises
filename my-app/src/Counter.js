6194ab49161e2e3c23342ba3ecd6c96ae7ef3b4d
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-01 16:38:53 +0100

    add:callback to setState

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -13,9 +13,9 @@ class Counter extends React.Component {
   //   Warning: Can't call setState on a component that is not yet mounted. This is a no-op, but it might indicate a bug in your application. Instead, assign to `this.state` directly or define a `state = {};` class property with the desired state in the Counter component.
   componentDidMount() {
     setInterval(() => {
-      this.setState({
+      this.setState(() => ({
         count: this.state.count + this.props.amount,
-      });
+      }));
     }, this.props.intervall);
   }
 
