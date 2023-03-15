49aef23f90248001ebd3dd4de2c6b1f06e95dabd
GitAbbonu <94009419+GitAbbonu@users.noreply.github.com>
2023-03-15 18:42:42 +0100

    Update DisplayLanguage.js

--- my-app/src/DisplayLanguage.js
+++ my-app/src/DisplayLanguage.js
@@ -1,5 +1,7 @@
 import { LanguageContext } from "./LanguageContext";
 
+//Create a LanguageContext and wrap a DisplayLanguage component within its provider. Add a select tag to the component containing the DisplayLanguage component that allows the user to select the language, and pass it as a value to the Provider.
+
 function DisplayLanguage() {
   const tradution = {
     en: {
