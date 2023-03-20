5f54771dcb25e992c4fca91dc68965bea1c1c81e
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-20 12:27:33 +0100

    add:exercise 55

--- my-app/src/useGitHubUser.js
+++ my-app/src/useGitHubUser.js
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
 
-// Extract the logic to fetch a Github user's data from the GithubUser component from useEffect 03 into a custom hook called useGithubUser.
+// Modify the useGithubUser hook to return the function to fetch the data of a Github user, along with the data of the user and the error and loading states.
 
 function useGitHubUser() {
   const [items, setItems] = useState(["Lista di nomi GitHub"]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function addToItems(list) {
     list.map((el) => getGitUser(el));
   }
 
   async function getGitUser(gitName) {
+    setLoading(true);
+
     try {
       const url = "https://api.github.com/users/" + gitName;
 
       const fet = await fetch(url);
       const json = await fet.json();
 
+      setLoading(false);
+
       setItems([...items, json.name]);
     } catch (err) {
-      console.log(err);
+      setError(error);
     }
   }
 
-  return { items, setItems, addToItems, getGitUser };
+  return { items, setItems, addToItems, getGitUser, loading, error };
 }
 
 export default useGitHubUser;
