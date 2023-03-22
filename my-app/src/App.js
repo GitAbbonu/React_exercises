761e0dcf19d60d659f77064209e4580b3d6bc903
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-22 19:57:00 +0100

    add:exercise 64

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import Counter from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
 import Welcome from "./Welcome";
 
-// Add three Links within the main App component and use them to navigate to all three routes.
+// Add a Not Found route that renders when a user navigates to a path that does not exist.
 function App() {
   return (
     <div>
@@ -17,6 +17,14 @@ function App() {
           <Route path="/users" element={<ShowGithubUser />} />
           <Route path="/users/:username" element={<ShowGithubUser />} />
           <Route path="/counter" element={<Counter />} />
+          <Route
+            path="*"
+            element={
+              <div>
+                <h1>404 Not Found</h1>
+              </div>
+            }
+          />
         </Routes>
       </nav>
     </div>
