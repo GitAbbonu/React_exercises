5ce9fbe934a208d128f113a7721f02d48a027d1e
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-23 20:26:09 +0100

    add:exercise 69

--- my-app/src/useGithubUser.js
+++ my-app/src/useGithubUser.js
@@ -1,12 +1,11 @@
 import useSWR from "swr";
 
-// Modify the useGithubUser hook so that, if the username is null, no request is made.
-
 function useGithubUser(username) {
   const { data, error, mutate } = useSWR(
     username ? `https://api.github.com/users/${username}` : null
   );
 
+  //   Modify the useGithubUser hook so that it returns a function to manually refetch the data when invoked.
   const fetchGithubUser = () => {
     mutate();
   };
